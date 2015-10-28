74a21c250be8c777c3c22216bb9cf6138e0ae55c
my9074 <313231068@qq.com>
2015-10-28 16:28:59 +0800

    Modify appmonitor.js

--- glance/js/app/appmonitor.js
+++ glance/js/app/appmonitor.js
@@ -17,54 +17,73 @@ function appMonitorCtrl($scope, $rootScope, $timeout, glanceHttp) {
 
     $scope.initCircle = function () {
         glanceHttp.ajaxGet(['metrics.appmonit', {
-            clusterID: $scope.appInfo.clusterId,
-            appName: $scope.appInfo.name
-        }], function (data) {
-            var cpuUseds = 0, cpuTotals = 0, memUseds = 0, memTotals = 0;
-            var cpuPercent, cpuText, memPercent, memText;
-            $scope.appStat = data.data;
-            if($scope.appStat){
+                clusterID: $scope.appInfo.clusterId,
+                appName: $scope.appInfo.name
+            }], function (data) {
+                var cpuUseds = 0, cpuTotals = 0, memUseds = 0, memTotals = 0;
+                var cpuPercent, cpuText, memPercent, memText;
+                $scope.appStat = data.data;
+                if ($scope.appStat.length === 0) {
                     for (var i = 0; i < $scope.appStat.length; i++) {
-                        $scope.appStat[i].memoryUsed = parseFloat(($scope.appStat[i].memoryUsed/ (1024*1024)).toFixed(2));
-                        $scope.appStat[i].memoryTotal = parseFloat(($scope.appStat[i].memoryTotal/ (1024*1024)).toFixed(2));
+                        if ($scope.appStat[i].cpuUsed && $scope.appStat[i].cpuTotal) {
+                            cpuUseds += $scope.appStat[i].cpuUsed;
+                            cpuTotals += $scope.appStat[i].cpuTotal;
+                        }
 
-                        cpuUseds += $scope.appStat[i].cpuUsed;
-                        cpuTotals += $scope.appStat[i].cpuTotal;
-                        memUseds += $scope.appStat[i].memoryUsed;
-                        memTotals += $scope.appStat[i].memoryTotal;
+                        if ($scope.appStat[i].memoryUsed && $scope.appStat[i].memoryTotal) {
+                            $scope.appStat[i].memoryUsed = parseFloat(($scope.appStat[i].memoryUsed / (1024 * 1024)).toFixed(2));
+                            $scope.appStat[i].memoryTotal = parseFloat(($scope.appStat[i].memoryTotal / (1024 * 1024)).toFixed(2));
+                            memUseds += $scope.appStat[i].memoryUsed;
+                            memTotals += $scope.appStat[i].memoryTotal;
+                        }
                     }
 
-                if (cpuUseds && cpuTotals) {
-                    cpuPercent = (cpuUseds / cpuTotals * 100).toFixed(2);
-                    cpuText = cpuPercent + "%";
-                } else {
-                    cpuPercent = 0;
-                    cpuText = "0%";
-                }
+                    if (cpuUseds && cpuTotals) {
+                        cpuPercent = (cpuUseds / cpuTotals * 100).toFixed(2);
+                        cpuText = cpuPercent + "%";
+                    } else {
+                        cpuPercent = 0;
+                        cpuText = "0%";
+                    }
 
-                if (memUseds && memTotals) {
-                    memPercent = (memUseds / memTotals * 100).toFixed(2);
-                    memText = memPercent + "%";
-                } else {
+                    if (memUseds && memTotals) {
+                        memPercent = (memUseds / memTotals * 100).toFixed(2);
+                        memText = memPercent + "%";
+                    } else {
+                        memPercent = 0;
+                        memText = "0%";
+                    }
+
+                    $('#appCpuStat').empty().removeData().attr({
+                        'data-percent': cpuPercent,
+                        'data-text': cpuText
+                    }).circliful();
+                    $('#appMemStat').empty().removeData().attr({
+                        'data-percent': memPercent,
+                        'data-text': memText
+                    }).circliful();
+                    promise = $timeout($scope.initCircle, 3000);
+                }
+                else {
                     memPercent = 0;
-                    memText = "0%";
+                    memText = "无数据";
+                    cpuPercent = 0;
+                    cpuText = "无数据";
+                    $('#appCpuStat').empty().removeData().attr({
+                        'data-percent': cpuPercent,
+                        'data-text': cpuText
+                    }).circliful();
+                    $('#appMemStat').empty().removeData().attr({
+                        'data-percent': memPercent,
+                        'data-text': memText
+                    }).circliful();
                 }
 
-                $('#appCpuStat').empty().removeData().attr({'data-percent': cpuPercent, 'data-text': cpuText}).circliful();
-                $('#appMemStat').empty().removeData().attr({'data-percent': memPercent, 'data-text': memText}).circliful();
-                promise = $timeout($scope.initCircle, 3000);
-            }else{
-                memPercent = 0;
-                memText = "无数据";
-                cpuPercent = 0;
-                cpuText = "无数据";
-                $('#appCpuStat').empty().removeData().attr({'data-percent': cpuPercent, 'data-text': cpuText}).circliful();
-                $('#appMemStat').empty().removeData().attr({'data-percent': memPercent, 'data-text': memText}).circliful();
+            }, undefined, null, function () {
+
             }
-
-        }, undefined, null, function () {
-
-        });
+        )
+        ;
     };
 
     $scope.$on('$destroy', function () {
