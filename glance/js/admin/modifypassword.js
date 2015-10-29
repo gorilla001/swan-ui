07eeec6e324995090aa65e71902f3b2e49534757
ainesmile <tshen@dataman-inc.com>
2015-10-29 16:12:33 +0800

    Feature add: midify password

---
+++ glance/js/admin/modifypassword.js
@@ -0,0 +1,23 @@
+function modifyPasswordCtrl($scope, $rootScope, glanceHttp, $window, Notification) {
+    $rootScope.show = '';
+    $scope.form = {
+        old_password: '',
+        new_password: '',
+        new_password_compare: ''
+    };
+    $scope.message_error_info = {};
+
+    $scope.modifyPassword = function(){
+        glanceHttp.ajaxFormPost($scope, ["auth.modifyPassword"], function() {
+            // Notification.success('密码修改成功！');
+            alert('密码修改成功！');
+            $window.history.back();
+        });
+    }
+
+    $scope.goBack = function() {
+        $window.history.back();
+    }
+}
+modifyPasswordCtrl.$inject = ['$scope', '$rootScope', 'glanceHttp', '$window', 'Notification'];
+glanceApp.controller('modifyPasswordCtrl', modifyPasswordCtrl);
