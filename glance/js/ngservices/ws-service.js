c50c35327f7c5c44175260d0ae0f61c834fc576a
Deshi Xiao <xiaods@gmail.com>
2015-11-06 02:52:16 +0800

    fix some js syntax error

--- glance/js/ngservices/ws-service.js
+++ glance/js/ngservices/ws-service.js
@@ -1,7 +1,8 @@
+/*global angular,glanceApp */
 function glanceWS($rootScope, ngSocket, utils, monitor) {
     var ws;
-    var wsCallbacks = {}
-    
+    var wsCallbacks = {};
+
     function buildWS(token) {
         ws = ngSocket(utils.buildFullURL(["ws.subscribe", {token: token}]));
         ws.onMessage(function (msg) {
@@ -10,8 +11,8 @@ function glanceWS($rootScope, ngSocket, utils, monitor) {
                 dataItems = [dataItems];
             }
             angular.forEach(dataItems, function (data) {
-                if (wsCallbacks[data["type"]]) {
-                    angular.forEach(wsCallbacks[data["type"]], function (callback) {
+                if (wsCallbacks[data.type]) {
+                    angular.forEach(wsCallbacks[data.type], function (callback) {
                         try{
                             callback(data.message);
                         } catch (e) {
@@ -21,42 +22,42 @@ function glanceWS($rootScope, ngSocket, utils, monitor) {
                 }
             });
         });
-    };
-    
+    }
+
     function wsOnMessage(callback, infoType) {
         if (!(infoType in wsCallbacks)) {
             wsCallbacks[infoType] = [];
         }
         wsCallbacks[infoType].push(callback);
-    };
+    }
 
     function listenMetrics() {
         wsOnMessage(function (data) {
             $rootScope.$broadcast("newNodeMetric-" + data.nodeId, data);
         }, SUB_INFOTYPE.nodeMetric);
-    };
-    
+    }
+
     function listenNodeStatus() {
         wsOnMessage(function (data) {
             $rootScope.$broadcast("nodeStatusUpdate-" + data.nodeId, data);
         }, SUB_INFOTYPE.nodeStatus);
-    };
-    
+    }
+
     var init = function (token) {
         buildWS(token);
         listenMetrics();
         listenNodeStatus();
     };
-    
+
     var clear = function () {
         ws.close();
         ws = null;
     };
-    
+
     return {
         init: init,
         clear: clear
-    }
+    };
 }
 glanceWS.$inject = ["$rootScope", "ngSocket", "utils", "monitor"];
 glanceApp.factory('glanceWS', glanceWS);
