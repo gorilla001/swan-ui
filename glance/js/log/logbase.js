b92ad7642f584b86442752a95ed86b7535ec3f9b
my9074 <313231068@qq.com>
2015-11-10 10:44:57 +0800

    Add Search Log context

--- glance/js/log/logbase.js
+++ glance/js/log/logbase.js
@@ -3,16 +3,15 @@
  */
 glanceApp.controller("logBaseCtrl", logBaseCtrl);
 
-logBaseCtrl.$inject = ['$scope', '$rootScope', 'glanceHttp', 'LogLoader'];
+logBaseCtrl.$inject = ['$scope', '$rootScope', 'glanceHttp', 'LogLoader', '$filter', '$timeout'];
 
-function logBaseCtrl($scope, $rootScope, glanceHttp, LogLoader) {
+function logBaseCtrl($scope, $rootScope, glanceHttp, LogLoader, $filter, $timeout) {
     $rootScope.show = "log";
-    moment.locale('zh-cn');
+    $scope.showContextUI = false;
+    var clusterIdTemp;
 
-    $scope.searchData = {
-        lte: new Date(),
-        gte: new Date((new Date()).getTime() - 60 * 60 * 1000)
-    };
+    $scope.lte = new Date();
+    $scope.gte = new Date((new Date()).getTime() - 60 * 60 * 1000);
 
     $scope.multiConfig = {
         selectAll: "全部选择",
@@ -21,10 +20,11 @@ function logBaseCtrl($scope, $rootScope, glanceHttp, LogLoader) {
         search: "查询匹配词",
         nothingSelected: "选择主机（ 可选 ）"
     };
-    
+
     $scope.inputNodesInfo = [];
 
     $scope.clusterlogs = new LogLoader();
+    $scope.contextlogs = new LogLoader();
 
     $scope.listCluster = function () {
         glanceHttp.ajaxGet(['cluster.listClusters'], function (data) {
@@ -35,7 +35,7 @@ function logBaseCtrl($scope, $rootScope, glanceHttp, LogLoader) {
     };
     $scope.listCluster();
 
-    $scope.$watch('searchData.clusterId', function (newValue, oldValue) {
+    $scope.$watch('clusterId', function (newValue, oldValue) {
         if (newValue) {
             getNodes(newValue);
             $scope.getAppOptions(newValue);
@@ -64,10 +64,83 @@ function logBaseCtrl($scope, $rootScope, glanceHttp, LogLoader) {
         });
     };
 
+    $scope.getLog = function () {
+        $scope.showContextUI = false;
+        $scope.searchData = {
+            'gte': $scope.gte,
+            'lte': $scope.lte,
+            'clusterId': $scope.clusterId,
+            'nodeId': $scope.nodeId,
+            'appName': $scope.appName,
+            'logSearchKey': $scope.logSearchKey
+        };
+        clusterIdTemp = $scope.searchData.clusterId;
+        $scope.clusterlogs.searchLogs($scope.searchData);
+    };
+
+    $scope.getContextLog = function (logInfo, indexId) {
+        $scope.curId = indexId;
+        var ip = logInfo.ip[0];
+        var taskid = logInfo.taskid[0];
+        var date = moment(logInfo.timestamp[0]).toDate();
+
+        function getContextLogTotal(times, total) {
+            times += 1;
+            addDate = moment(logInfo.timestamp[0]).add(120 * times, 's').toDate();
+            subDate = moment(logInfo.timestamp[0]).subtract(120 * times, 's').toDate();
+            var startDate = $filter('date')(subDate, 'yyyy-MM-ddTHH:mm:ss');
+            var endDate = $filter('date')(addDate, 'yyyy-MM-ddTHH:mm:ss');
+            $scope.searchData = {
+                'gte': startDate,
+                'lte': endDate,
+                'nodeId': new Array(ip),
+                'instanceName': taskid,
+                'clusterId': clusterIdTemp,
+                'size': 0
+            };
+            $scope.contextlogs.searchLogs($scope.searchData, function (logSize) {
+                if (logSize < total && times < 6) {
+                    getContextLogTotal(times, total);
+                } else {
+                    $scope.searchData = {
+                        'gte': startDate,
+                        'lte': endDate,
+                        'nodeId': new Array(ip),
+                        'instanceName': taskid,
+                        'clusterId': clusterIdTemp,
+                        'size': $scope.contextlogs.logSize
+                    };
+                    $scope.contextlogs.searchLogs($scope.searchData,function(){
+                        $timeout(function(){
+                            var scrollHeight = 0;
+                            var Odiv = document.getElementById("contextLog");
+                            var Oli = document.getElementsByClassName("list-unstyled")[1].getElementsByTagName("li");
+                            for (var i = 0; i < Oli.length; i++) {
+                                scrollHeight += Oli[i].offsetHeight;
+                                if (Oli[i].className.indexOf('active') > 0) {
+                                    break;
+                                }
+                            }
+                            Odiv.scrollTop = scrollHeight - Odiv.offsetHeight / 2;
+                        }, 250);
+                    });
+                }
+            });
+        }
+
+        getContextLogTotal(0, 100);
+        $scope.showContextUI = true;
+    };
+
+    $scope.returnLos = function () {
+        $scope.showContextUI = false;
+    };
+
     //datetimepicker option
     $scope.dateOptions = {
         startingDay: 1,
         showWeeks: false
     };
+
     $scope.showMeridian = false;
 }
