67f1397aa9a61be88dfdfe56c3de1986d5ef437a
ainesmile <tshen@dataman-inc.com>
2015-12-03 10:45:07 +0800

    call list cluster interface to get clusters inofs, and display them

--- glance/js/dynamic/dynamicbase.js
+++ glance/js/dynamic/dynamicbase.js
@@ -1,12 +1,37 @@
 /**
  * Created by myu on 15-9-22.
  */
-glanceApp.controller("dynamicBaseCtrl", dynamicBaseCtrl);
+(function() {
+    'use strict';
+    glanceApp.controller('dynamicBaseCtrl', dynamicBaseCtrl);
 
-dynamicBaseCtrl.$inject = ['$scope', '$rootScope', 'glanceHttp'];
+    dynamicBaseCtrl.$inject = ['$scope', '$rootScope', 'glanceHttp'];
 
-function dynamicBaseCtrl($scope, $rootScope, glanceHttp) {
-    $rootScope.show = "dynamic";
-    $scope.list = [1,2,3,4]
+    function dynamicBaseCtrl($scope, $rootScope, glanceHttp) {
+        $rootScope.show = 'dynamic';
 
-}
+        $scope.clusterList = [];
+
+        (function listAllClusters() {
+            glanceHttp.ajaxGet(['cluster.clusters'], function (data) {
+                if (data && data.data) {
+                    $scope.clusterList = collectClusterList(data.data);
+                }
+            });
+        })();
+
+        function collectClusterList(clusters) {
+            var clusterList = [];
+            var cluster;
+            for (var i = 0; i < clusters.length; i++) {
+                cluster = clusters[i];
+                clusterList[i] = {
+                    id: cluster.id,
+                    name: cluster.name,
+                    nodes: cluster.nodes
+                };
+            }
+            return clusterList;
+        }
+    }
+})();
