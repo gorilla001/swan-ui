d3a4646ebb2f5a1466af3a57d7bdcc0e05213fcc
ainesmile <tshen@dataman-inc.com>
2015-12-07 14:45:12 +0800

    test for updateClusterCache in service `groupNodes` when node status update

--- glance/test/unit/services/groupnode.spec.js
+++ glance/test/unit/services/groupnode.spec.js
@@ -56,6 +56,113 @@
         });
 
         describe('updateClusterCache', function() {
+            it('should update cluster caches if node status updated', function() {
+                var wsData = {
+                    status: 'running',
+                    clusterId: 82,
+                    nodeId: '6a82ce517a4c4dfe82fcae0d92f9bb4d'
+                };
+                var clusters = [
+                    {
+                        cluster_type: '1_master',
+                        created_at: '2015-12-02 15:18:01',
+                        id: 82,
+                        name: '111',
+                        nodes: [
+                            {
+                                agent_version: 'v0.1.112600',
+                                attributes: [],
+                                created_at: '2015-12-03 14:11:40',
+                                id: 'nodeId1',
+                                ip: '10.3.10.40',
+                                name: '10.3.10.40',
+                                role: 'master',
+                                services: [
+                                    {name: 'bamboo_gateway', status: 'uninstalled'},
+                                    {name: 'bamboo_proxy', status: 'uninstalled'},
+                                    {name: 'cadvisor', status: 'running'},
+                                    {name: 'exhibitor', status: 'running'},
+                                    {name: 'logcollection', status: 'running'},
+                                    {name: 'marathon', status: 'running'},
+                                    {name: 'master', status: 'failed'},
+                                    {name: 'slave', status: 'failed'},
+                                    {name: 'zookeeper', status: 'running'}
+                                ],
+                                status: 'running',
+                                updated_at: '2015-12-07 11:22:28'
+                            }
+                        ],
+                        status: 'running',
+                        updated_at: '2015-12-02 15:19:31'
+                    },{
+                        cluster_type: '1_master',
+                        created_at: '2015-12-02 15:18:01',
+                        id: 83,
+                        name: '222',
+                        nodes: [
+                            {
+                                agent_version: 'v0.1.112600',
+                                attributes: [],
+                                created_at: '2015-12-03 14:11:40',
+                                id: 'nodeId2',
+                                ip: '10.3.10.40',
+                                name: '10.3.10.40',
+                                role: 'master',
+                                services: [
+                                    {name: 'bamboo_gateway', status: 'uninstalled'},
+                                    {name: 'bamboo_proxy', status: 'uninstalled'},
+                                    {name: 'cadvisor', status: 'running'},
+                                    {name: 'exhibitor', status: 'running'},
+                                    {name: 'logcollection', status: 'running'},
+                                    {name: 'marathon', status: 'running'},
+                                    {name: 'master', status: 'failed'},
+                                    {name: 'slave', status: 'failed'},
+                                    {name: 'zookeeper', status: 'running'}
+                                ],
+                                status: 'running',
+                                updated_at: '2015-12-07 11:22:28'
+                            }
+                        ],
+                        status: 'running',
+                        updated_at: '2015-12-02 15:19:31'
+                    }
+                ];
+                var clusterCache = {
+                    nodeStatusCache: {'6a82ce517a4c4dfe82fcae0d92f9bb4d': "terminated"},
+                    rawStatusCache: {'6a82ce517a4c4dfe82fcae0d92f9bb4d': "terminated"},
+                    servicesCache: {
+                        '6a82ce517a4c4dfe82fcae0d92f9bb4d': [
+                            {name: 'bamboo_gateway', status: 'uninstalled'},
+                            {name: 'bamboo_proxy', status: 'uninstalled'},
+                            {name: 'cadvisor', status: 'running'},
+                            {name: 'exhibitor', status: 'running'},
+                            {name: 'logcollection', status: 'running'},
+                            {name: 'marathon', status: 'running'},
+                            {name: 'master', status: 'failed'},
+                            {name: 'slave', status: 'running'},
+                            {name: 'zookeeper', status: 'running'}
+                        ]
+                    },
+                    amounts: {
+                        failed: 0,
+                        initing: 0,
+                        installing: 0,
+                        running: 0,
+                        terminated: 1,
+                        upgrading: 0
+                    }
+                };
+
+                var updetedClusterCaches = groupNodes.updateClusterCache(clusters, wsData, clusterCache);
+
+                expect(updetedClusterCaches.newAmounts.terminated).toEqual(0);
+                expect(updetedClusterCaches.newAmounts.running).toEqual(1);
+
+            });
+
+            it('should update cluster caches if node services updated', function() {
+
+            });
 
         });
 
