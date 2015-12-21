817e1e8f981aef820923d0a899e729a44aab88f2
jfchen <jfchen@dataman-inc.com>
2015-12-21 16:25:15 +0800

    add feature: push user_id to growingio

--- glance/js/rootctrl.js
+++ glance/js/rootctrl.js
@@ -11,6 +11,21 @@ function rootCtrl($scope, $rootScope, $state, glanceUser, glanceHttp) {
         $rootScope.userId = data.data["userId"];
         $rootScope.isSuperuser = data.data["isSuperuser"];
         $rootScope.latestVersion = data.data["latestVersion"]
+        
+        //GrowingIO
+        if (RUNNING_ENV === "prod") {
+            (function(){ 
+                _vds.push(['setAccountId', '0edf12ee248505950b0a77b02d47c537']); 
+                _vds.push(['setCS1', 'user_id',  data.data["userId"].toString()]);
+                (function() {
+                    var vds = document.createElement('script'); 
+                    vds.type='text/javascript'; vds.async = true;
+                    vds.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'dn-growing.qbox.me/vds.js';
+                    var s = document.getElementsByTagName('script')[0];
+                    s.parentNode.insertBefore(vds, s);
+                })();
+            })();
+        }
     });
 
     // app list request params
