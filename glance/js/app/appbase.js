f579a062995a31e315c869cdf354dbf84922aa59
my9074 <313231068@qq.com>
2016-01-04 10:37:56 +0800

    1.Modify createapp Advanced Settings url 2.Modify appdetail menu logic 3.Modify appstate flag for appbase.js

--- glance/js/app/appbase.js
+++ glance/js/app/appbase.js
@@ -27,7 +27,8 @@ function appBaseCtrl($scope, $rootScope, $state, $timeout, glanceHttp, Notificat
         '5': "删除中",
         '6': "扩展中",
         '7': "启动中",
-        '8': "撤销中"
+        '8': "撤销中",
+        '9': "失联"
     };
 
     $scope.portType = {
