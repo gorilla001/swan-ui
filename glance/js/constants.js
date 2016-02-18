e8aa2eeffa761ea3dda584aaeb8f204930eacc83
my9074 <313231068@qq.com>
2016-02-18 17:37:29 +0800

    Update interface for download logs

--- glance/js/constants.js
+++ glance/js/constants.js
@@ -87,8 +87,9 @@ BACKEND_URL = {
     },
     log: {
         search: "es/index",
-        download: "es/index/download/log.json",
-        searchContext: "es/context"
+        downloadSearch: "es/index/download",
+        searchContext: "es/context",
+        downloadContext: "es/context/download"
     },
     app: {
         list: "api/v1/applications/",
