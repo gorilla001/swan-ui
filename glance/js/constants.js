7fe03564f90e2de84c234d83bdf22f209b588883
Ting Shen <tshen@dataman-inc.com>
2016-03-01 17:34:22 +0800

    modiry app interfaces addresses

--- glance/js/constants.js
+++ glance/js/constants.js
@@ -91,31 +91,12 @@ BACKEND_URL = {
         downloadContext: "es/context/download"
     },
     app: {
-        list: "api/v1/applications/",
-        allList:"api/v1/applications/",
-        deploy: "api/v1/applications/deploy",
-        instances: "api/v1/applications/$app_id/instances",
-        info: "api/v1/applications/$app_id",
-        config: "api/v1/applications/$app_id/config",
-        upContainerNum:"api/v1/applications/update-container-num",
-        deleteApp:"api/v1/applications/$app_id/delete",
-        stop:"api/v1/applications/$app_id/stop",
-        start:"api/v1/applications/$app_id/start",
-        options:"api/v1/applications/$cluster_id/options",
-        imageVersions:"api/v1/applications/$app_id/image-versions",
-        versionDeploy:"api/v1/applications/$app_versionId/version-deploy",
-        updateVersion:"api/v1/applications/update-version",
-        isdeploying: "api/v1/applications/$app_id/isdeploying",
-        cancelDeploy: "api/v1/applications/$app_id/cancel-deployment",
-        undoScaling: "api/v1/applications/$app_id/cancel-scaling",
-        getNodes: "api/v1/applications/$clusterId/get-appnodes?cluster_id=$cluster_id&app_name=$app_name",
-        getNodePorts: "api/v1/applications/$clusterId/get-appnodes-port?cluster_id=$cluster_id&app_aliase=$app_aliase",
-        event: "api/v1/applications/$app_id/events?page=$page",
-        ports: "api/v1/applications/$cluster_id/ports",
-        getAppsStatus: "api/v1/applications/0/appStatus",
-        getVersionConfig:"api/v1/applications/$app_versionId/version-config",
-        deleteVersion:"api/v1/applications/$app_versionId/version-delete",
-        resend: "api/v1/applications/$app_id/redeploy"
+        userApps: '/api/v3/apps',
+        clusterApps: '/api/v3/clusters/$cluster_id/apps',
+        clusterApp: '/api/v3/clusters/$cluster_id/apps/$app_id',
+        appEvent: '/api/v3/clusters/$cluster_id/apps/$app_id/events',
+        appVersions: '/api/v3/clusters/$cluster_id/apps/$app_id/versions',
+        appVersion: '/api/v3/clusters/$cluster_id/apps/$app_id/versions/$version_id'
     }
         
 };
