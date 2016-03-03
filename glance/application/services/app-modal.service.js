a00f27ec00e4c3caa6d333571feb7b43e5b41841
jfchen <jfchen@dataman-inc.com>
2016-03-03 14:14:01 +0800

    fix bug: add app-modal.service.js

---
+++ glance/application/services/app-modal.service.js
@@ -0,0 +1,48 @@
+/**
+ * Created by my9074 on 16/3/2.
+ */
+(function () {
+    'use strict';
+    angular.module('glance.app')
+        .factory('appModal', appModal);
+
+    appModal.$inject = ['$uibModal', 'appcurd'];
+
+    function appModal($uibModal, appcurd) {
+        return {
+            openUpContainerModal: openUpContainerModal
+        };
+
+        function openUpContainerModal(instanceNum, clusterId, appId) {
+            var modalInstance = $uibModal.open({
+                templateUrl: '/application/common_app_views/upContainerModal.html',
+                controller: _UpContainerCtrl,
+                controllerAs: 'upContainerCtrl',
+                resolve: {
+                    instanceNum: function () {return instanceNum},
+                    clusterId: function () {return clusterId},
+                    appId: function () {return appId}
+                }
+            })
+            
+            return modalInstance;
+        }
+        
+        _UpContainerCtrl.$inject = ['$uibModalInstance', 'instanceNum', 'clusterId', 'appId'];
+        
+        function _UpContainerCtrl($uibModalInstance, instanceNum, clusterId, appId) {
+            var self = this;
+            self.instanceNum = instanceNum;
+            this.updateContainer = function () {
+                appcurd.updateContainer({instances: self.instanceNum}, clusterId, appId).then(function () {
+                });
+                $uibModalInstance.close();
+            };
+            
+            this.cancel = function () {
+                $uibModalInstance.close();
+            }
+            
+        }
+    }
+})();
