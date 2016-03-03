f851e2d58c2c23bdca350583b76464026f3b6a41
Ting Shen <tshen@dataman-inc.com>
2016-03-03 17:39:49 +0800

    add service `appLabelService`

---
+++ glance/application/services/app-label.service.js
@@ -0,0 +1,57 @@
+(function () {
+    'use strict';
+    angular.module('glance.app')
+        .factory('appLabelService', appLabelService);
+
+    appLabelService.$inject = ['clusterBackendService'];
+
+    function appLabelService(clusterBackendService) {
+        return {
+            listClusterLabels: listClusterLabels,
+            listNodesByLabelIds: listNodesByLabelIds
+        };
+
+        /////////////////
+
+        function listClusterLabels(nodes) {
+            var labels = [];
+            var labelsNames = [];
+            angular.forEach(nodes, function(node, index) {
+                angular.forEach(node.node_labels, function(label, labelIndex){
+                    if (labelsNames.indexOf(label.label.name) === -1) {
+                        labelsNames.push(label.label.name);
+                        labels.push({id: label.label.id, name: label.label.name});
+                    }
+                });
+            });
+            return labels;
+        }
+
+        function  listNodesByLabelIds(selectedLables, clusterId) {
+            var labelIds = collectIds(selectedLables);
+            var labelIdsString = concatString(labelIds);
+            return clusterBackendService.listNodesByLabelIds(clusterId, labelIdsString);
+        }
+        
+        //////////////
+
+        function collectIds(items) {
+            var ids = [];
+            angular.forEach(items, function(item, index) {
+                ids.push(item.id);
+            });
+            return ids;
+        }
+
+        function concatString(array) {
+            var string = '';
+            angular.forEach(array, function(item, index) {
+                string += item + ',';
+            });
+            return string;
+        }
+
+
+        
+    }
+})();
