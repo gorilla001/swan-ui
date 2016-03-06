f593474365ae9e6aad145e35a556a07047e5b95c
my9074 <313231068@qq.com>
2016-03-06 11:32:53 +0800

    Add createProject for image module

--- glance/image/create/create.ctrl.js
+++ glance/image/create/create.ctrl.js
@@ -3,10 +3,52 @@
     angular.module('glance.image')
         .controller('ImageCreateCtrl', ImageCreateCtrl);
 
-    ImageCreateCtrl.$inject = [];
+    ImageCreateCtrl.$inject = ['imageservice'];
 
-    function ImageCreateCtrl() {
+    function ImageCreateCtrl(imageservice) {
         var self = this;
         ///
+        self.form = {
+            uid: 0,
+            id: 0,
+            parentId: 0,
+            name: "",
+            repoUri: "",
+            triggerType: 0,
+            active: true,
+            versions: 0,
+            autoBuild: 'true',
+            tag: true,
+            branch: false,
+            autoTime: 5
+        };
+
+        self.options = [
+            {
+                name: '5 分钟',
+                value: 5
+            },
+            {
+                name: '10 分钟',
+                value: 10
+            }
+        ];
+
+        self.triggerCount = 1;
+
+        self.createProject = function (fromData) {
+            console.log(self.form);
+            //imageservice.createProject(fromData).then(function (data) {
+            //    ////
+            //})
+        };
+
+        self.triggerCheck = function (checkValue) {
+            if (checkValue) {
+                self.triggerCount++;
+            } else {
+                self.triggerCount--;
+            }
+        };
     }
 })();
--- glance/image/home/home.ctrl.js
+++ glance/image/home/home.ctrl.js
@@ -3,12 +3,26 @@
     angular.module('glance.image')
         .controller('ImageHomeCtrl', ImageHomeCtrl);
 
-    ImageHomeCtrl.$inject = ['$rootScope'];
+    ImageHomeCtrl.$inject = ['$rootScope', 'imageservice'];
 
-    function ImageHomeCtrl($rootScope) {
+    function ImageHomeCtrl($rootScope, imageservice) {
         var self = this;
         
         $rootScope.show = 'image';
-        ///
+
+        listProjets();
+
+        self.deleteProject = function(projectId){
+            ///
+        };
+
+        function listProjets(){
+            imageservice.listProjects().then(function(data){
+                self.projects = data;
+            })
+        }
+
+
+
     }
 })();
