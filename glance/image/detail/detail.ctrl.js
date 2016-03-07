e282c7df477b9df88fcfa50fc5f47d54b09476d4
Ting Shen <tshen@dataman-inc.com>
2016-03-07 15:56:36 +0800

    add button to copy command of pull image

--- glance/image/detail/detail.ctrl.js
+++ glance/image/detail/detail.ctrl.js
@@ -3,10 +3,11 @@
     angular.module('glance.image')
         .controller('ImageDetailCtrl', ImageDetailCtrl);
 
-    ImageDetailCtrl.$inject = [];
+    ImageDetailCtrl.$inject = ['utils'];
 
-    function ImageDetailCtrl() {
+    function ImageDetailCtrl(utils) {
         var self = this;
-        ///
+        utils.clickToCopy();
+        
     }
 })();
