a8a046e1987b31d12635b142b961b493b5591376
jfchen <jfchen@dataman-inc.com>
2016-03-09 18:45:20 +0800

    feature: add group_id in create cluster

--- glance/js/cluster/createclusterformctrl.js
+++ glance/js/cluster/createclusterformctrl.js
@@ -1,8 +1,16 @@
-function createClusterFormCtrl($scope, $state, gHttp) {
+function createClusterFormCtrl($scope, $state, gHttp, userBackend) {
     $scope.form = {
         clusterType: '1_master'
     };
+    userBackend.listGroups().then(function(data) {
+        $scope.groups=data.groups;
+    });
     $scope.createCluster = function (isAddNode) {
+        if (!$scope.form.groupId) {
+            delete $scope.form.groupId;
+        } else {
+            $scope.form.groupId = parseInt($scope.form.groupId)
+        }
         gHttp.Resource('cluster.clusters').post($scope.form, {'form': $scope.staticForm}).then(function (data) {
             if (isAddNode) {
                 $state.go('cluster.nodesource', {'clusterId': data.id});
@@ -13,5 +21,5 @@ function createClusterFormCtrl($scope, $state, gHttp) {
     };
 }
 
-createClusterFormCtrl.$inject = ["$scope", "$state", "gHttp"];
+createClusterFormCtrl.$inject = ["$scope", "$state", "gHttp", "userBackend"];
 glanceApp.controller("createClusterFormCtrl", createClusterFormCtrl);
