799cbb03cd60ad9d7c7ee3b51980d67d27dfadb7
my9074 <313231068@qq.com>
2016-03-29 16:34:37 +0800

    Add dynamic fields for repository

--- glance/glance/repository/list/list.ctrl.js
+++ glance/glance/repository/list/list.ctrl.js
@@ -8,6 +8,50 @@
     function RepoListCtrl() {
         var self = this;
 
-        /////
+        self.fields =
+            [
+                {
+                    type: 'string',
+                    default: '1111',
+                    label: 'Name',
+                    descrition: 'demo demo'
+                },
+                {
+                    type: 'int',
+                    default: 300,
+                    label: '22222',
+                    descrition: 'demo demo'
+                },
+                {
+                    type: 'boolean',
+                    default: true,
+                    label: 'Animal',
+                    descrition: 'boolean boolean'
+                },
+                {
+                    type: 'multiline',
+                    default: '1231332',
+                    label: 'textarea',
+                    descrition: 'demo demo'
+                },
+                {
+                    type: 'enum',
+                    default: 'a',
+                    label: 'enum',
+                    descrition: 'demo demo',
+                    options: ['a', 'b']
+                },
+                {
+                    type: 'service',
+                    label: 'service',
+                    descrition: 'demo demo'
+                },
+                {
+                    type: 'password',
+                    label: 'password',
+                    default: 1234123,
+                    descrition: 'demo demo'
+                }
+            ];
     }
 })();
