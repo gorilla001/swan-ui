a4877505060cbdec333f4aff573b18d3fa5b7f5b
my9074 <313231068@qq.com>
2016-03-31 10:28:24 +0800

    Add static html for repository list

--- glance/glance/repository/detail/detail.ctrl.js
+++ glance/glance/repository/detail/detail.ctrl.js
@@ -8,6 +8,51 @@
     function RepoDetailCtrl() {
         var self = this;
 
-        /////
+        self.markdown = '###Markdown directive *It works!*';
+        self.fields =
+            [
+                {
+                    type: 'string',
+                    default: '1111',
+                    label: 'Name',
+                    descrition: 'demo demo'
+                },
+                {
+                    type: 'int',
+                    default: 300,
+                    label: '22222',
+                    descrition: 'demo demo'
+                },
+                {
+                    type: 'boolean',
+                    default: true,
+                    label: 'Animal',
+                    descrition: 'boolean boolean'
+                },
+                {
+                    type: 'multiline',
+                    default: '1231332',
+                    label: 'textarea',
+                    descrition: 'demo demo'
+                },
+                {
+                    type: 'enum',
+                    default: 'a',
+                    label: 'enum',
+                    descrition: 'demo demo',
+                    options: ['a', 'b']
+                },
+                {
+                    type: 'service',
+                    label: 'service',
+                    descrition: 'demo demo'
+                },
+                {
+                    type: 'password',
+                    label: 'password',
+                    default: 1234123,
+                    descrition: 'demo demo'
+                }
+            ];
     }
 })();
--- glance/glance/repository/list/list.ctrl.js
+++ glance/glance/repository/list/list.ctrl.js
@@ -5,55 +5,29 @@
 
 
     /* @ngInject */
-    function RepoListCtrl() {
+    function RepoListCtrl(repoBackend) {
         var self = this;
+        ////
 
+        activate();
 
-        self.markdown = '###Markdown directive *It works!*';
-        self.fields =
-            [
-                {
-                    type: 'string',
-                    default: '1111',
-                    label: 'Name',
-                    descrition: 'demo demo'
-                },
-                {
-                    type: 'int',
-                    default: 300,
-                    label: '22222',
-                    descrition: 'demo demo'
-                },
-                {
-                    type: 'boolean',
-                    default: true,
-                    label: 'Animal',
-                    descrition: 'boolean boolean'
-                },
-                {
-                    type: 'multiline',
-                    default: '1231332',
-                    label: 'textarea',
-                    descrition: 'demo demo'
-                },
-                {
-                    type: 'enum',
-                    default: 'a',
-                    label: 'enum',
-                    descrition: 'demo demo',
-                    options: ['a', 'b']
-                },
-                {
-                    type: 'service',
-                    label: 'service',
-                    descrition: 'demo demo'
-                },
-                {
-                    type: 'password',
-                    label: 'password',
-                    default: 1234123,
-                    descrition: 'demo demo'
-                }
-            ];
+        function activate() {
+            listRepository();
+            listCategories();
+        }
+
+        function listRepository() {
+            repoBackend.listRepositories()
+                .then(function (data) {
+                    self.repositories = data
+                })
+        }
+
+        function listCategories() {
+            repoBackend.listCategories()
+                .then(function (data) {
+                    self.categories = data
+                })
+        }
     }
 })();
