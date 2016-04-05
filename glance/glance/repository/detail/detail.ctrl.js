e408176182d588660b4e224cf291d92391e6a83f
my9074 <313231068@qq.com>
2016-04-05 10:15:20 +0800

    Update repo detail form

--- glance/glance/repository/detail/detail.ctrl.js
+++ glance/glance/repository/detail/detail.ctrl.js
@@ -3,14 +3,16 @@
     angular.module('glance.repository')
         .controller('RepoDetailCtrl', RepoDetailCtrl);
 
-
     /* @ngInject */
     function RepoDetailCtrl($stateParams, repoBackend, $base64) {
         var self = this;
         var projectName = $stateParams.projectName;
         var repositoryName = $stateParams.repositoryName;
 
-        self.repoDetail = {};
+        self.form = {
+            name: '',
+            category: ''
+        };
         self.tags = [];
         self.deploy = deploy;
 
@@ -24,8 +26,11 @@
         function getRepoDetail(projectName, repositoryName) {
             repoBackend.getRepository(projectName, repositoryName)
                 .then(function (data) {
-                    self.repoDetail = data;
-                    self.repoDetail.markdown = decodeURIComponent(escape($base64.decode(self.repoDetail.markdown)));
+                    self.form = data;
+                    self.markdown = decodeURIComponent(escape($base64.decode(self.form.markdown)));
+                    if (data.sryCompose) {
+                        self.form.questions = angular.fromJson(data.sryCompose)['.catalog'].questions;
+                    }
                 })
         }
 
@@ -36,65 +41,9 @@
                 })
         }
 
-        function deploy(){
+        function deploy() {
             ///
+            console.log(self.form)
         }
-
-        self.fields = [
-            {
-                "variable": "remote_driver",
-                "type": "enum",
-                "required": true,
-                "label": "Remote Driver",
-                "description": "Remote Git and Auth scheme",
-                "options": [
-                    "github",
-                    "bitbucket",
-                    "gitlab",
-                    "gogs"
-                ]
-            },
-            {
-                "variable": "remote_config",
-                "type": "password",
-                "required": true,
-                "label": "Remote Config",
-                "description": "Must be the full connection string. see http://readme.drone.io/setup/overview/ for more info"
-            },
-            {
-                "variable": "public_port",
-                "type": "int",
-                "required": true,
-                "label": "Public Port",
-                "description": "Port that the load balancer will listen on. Must be accessible to remote driver for webhooks"
-            },
-            {
-                "variable": "database_driver",
-                "type": "enum",
-                "label": "Database Driver",
-                "description": "Database backend to use.",
-                "required": true,
-                "default": "sqlite3",
-                "options": [
-                    "sqlite3",
-                    "postgres",
-                    "mysql"
-                ]
-            },
-            {
-                "variable": "database_config",
-                "label": "Database Config",
-                "type": "password",
-                "required": true,
-                "description": "Must be full db string. The hostname for the DB will be 'database'. See http://readme.drone.io/setup/overview/ for more info"
-            },
-            {
-                "variable": "database_service",
-                "type": "service",
-                "label": "Database Service",
-                "description": "Service to link to for database. For instance if using the default Rancher Galera cluster select galera/galera-lb.",
-                "default": "drone-server"
-            }
-        ];
     }
 })();
