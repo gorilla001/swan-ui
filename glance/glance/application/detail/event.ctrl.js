df5489534f84c5a01eb240a302681f01c2a3b092
jfchen <jfchen@dataman-inc.com>
2016-05-04 14:46:55 +0800

    fix: SRYD-594 add per_page in list app.events

--- glance/glance/application/detail/event.ctrl.js
+++ glance/glance/application/detail/event.ctrl.js
@@ -14,6 +14,7 @@
         self.events = [];
         self.currentTypeText = APP_EVENTS_MSG;
         self.eventTypeText = APP_EVENTS_TYPE;
+        self.pageLength = 20;
 
         self.appEvent = function () {
             getEvents(1)
@@ -30,11 +31,10 @@
         self.appEvent();
 
         function getEvents(page) {
-            appservice.listAppEvents({page: page}, $stateParams.cluster_id, $stateParams.app_id).then(function (data) {
+            appservice.listAppEvents({page: page, per_page: self.pageLength}, $stateParams.cluster_id, $stateParams.app_id).then(function (data) {
                 self.events = data.Message;
 
                 self.totalItems = data.Page;
-                self.pageLength = 20;
                 self.showPagination = (self.totalItems > self.pageLength);
             });
         }
