43eb3f0659f7f00d689683c1ce49afe65d60de95
my9074 <313231068@qq.com>
2016-05-13 14:33:33 +0800

    Modify policy of appwarning list to Material Style

--- glance/glance/policy/policy.route.js
+++ glance/glance/policy/policy.route.js
@@ -10,21 +10,18 @@
         $stateProvider
             .state('policy', {
                 url: '/policy',
-                abstract: true,
-                views: {
-                    '': {
-                        templateUrl: '/glance/policy/common/policy.html'
-                    }
-                }
+                template: '<ui-view/>',
+                targetState: 'tab'
             })
-            .state('policyWarningCreate', {
+            .state('policy.tab', {
+                url: '/policy',
+                templateUrl: '/glance/policy/common/policy.html',
+                targetState: 'appwarning'
+            })
+            .state('policy.WarningCreate', {
                 url: '/appwarningcreate',
-                views: {
-                    '': {
-                        templateUrl: '/glance/policy/appwarning/createupdate/create-update.html',
-                        controller: 'CreateWarningCtrl as createWarningCtrl'
-                    }
-                },
+                templateUrl: '/glance/policy/appwarning/createupdate/create-update.html',
+                controller: 'CreateWarningCtrl as createWarningCtrl',
                 resolve: {
                     target: function () {
                         return 'create'
@@ -34,14 +31,10 @@
                     }
                 }
             })
-            .state('policyWarningUpdate', {
+            .state('policy.WarningUpdate', {
                 url: '/appwarningupdate?task_id',
-                views: {
-                    '': {
-                        templateUrl: '/glance/policy/appwarning/createupdate/create-update.html',
-                        controller: 'CreateWarningCtrl as createWarningCtrl'
-                    }
-                },
+                templateUrl: '/glance/policy/appwarning/createupdate/create-update.html',
+                controller: 'CreateWarningCtrl as createWarningCtrl',
                 resolve: {
                     target: function () {
                         return 'update'
@@ -49,39 +42,48 @@
                     warning: getTaskInfo
                 }
             })
-            .state('policy.appwarning', {
+            .state('policy.tab.appwarning', {
                 url: '/appwarning',
                 views: {
-                    'policyTab': {
+                    'appWarningTab': {
                         templateUrl: '/glance/policy/appwarning/warning-tab-view.html'
                     }
-                }
+                },
+                targetState: 'warninglist'
             })
-            .state('policy.appwarning.warninglist', {
+            .state('policy.tab.appwarning.warninglist', {
                 url: '/warninglist?per_page&page&order&keywords&sort_by&appalias',
                 views: {
-                    'warningContent': {
+                    'list': {
                         templateUrl: '/glance/policy/appwarning/list/list.html',
                         controller: 'WarningListCtrl as warningListCtrl'
                     }
+                },
+                defaultParams: {
+                    per_page: 20,
+                    page: 1
                 },
                 resolve: {
                     data: warningList
                 }
             })
-            .state('policy.appwarning.warningevent', {
+            .state('policy.tab.appwarning.warningevent', {
                 url: '/warningevent?per_page&page&order&keywords&sort_by',
                 views: {
-                    'warningContent': {
+                    'event': {
                         templateUrl: '/glance/policy/appwarning/event/event.html',
                         controller: 'WarningEventCtrl as warningEventCtrl'
                     }
+                },
+                defaultParams: {
+                    per_page: 20,
+                    page: 1
                 },
                 resolve: {
                     data: warningEvent
                 }
             })
-            .state('policy.apptimescaling', {
+            .state('policy.tab.apptimescaling', {
                 url: '/apptimescaling',
                 views: {
                     'policyTab': {
@@ -89,13 +91,17 @@
                     }
                 }
             })
-            .state('policy.apptimescaling.scalinglist', {
+            .state('policy.tab.apptimescaling.scalinglist', {
                 url: '/scalinglist?per_page&page&order&keywords&sort_by',
                 views: {
                     'scalingContent': {
                         templateUrl: '/glance/policy/apptimescaling/list/list.html',
                         controller: 'ScalingListCtrl as scalingListCtrl'
                     }
+                },
+                defaultParams: {
+                    per_page: 20,
+                    page: 1
                 },
                 resolve: {
                     data: scaleList
@@ -187,14 +193,15 @@
 
     /* @ngInject */
     function warningList(appWarningBackend, utils, $stateParams) {
-        return appWarningBackend.warningList(utils.encodeQueryParams($stateParams));
+        //return appWarningBackend.warningList(utils.encodeQueryParams($stateParams));
+        return {tasks: [{id:1, appname:"111", duration: 1,times:2, emails:"3@3.com",updated: "2016-05-09T14:33:48Z", enabled: true}],count:6}
     }
 
     /* @ngInject */
     function getTaskInfo(appWarningBackend, $stateParams) {
         return appWarningBackend.getWarning($stateParams.task_id);
     }
-    
+
     /* @ngInject */
     function getLogPolicyInfo(logWarningBackend, $stateParams) {
         return logWarningBackend.getLogPolicy($stateParams.log_id);
@@ -202,7 +209,8 @@
 
     /* @ngInject */
     function warningEvent(appWarningBackend, utils, $stateParams) {
-        return appWarningBackend.warningEvent(utils.encodeQueryParams($stateParams));
+        //return appWarningBackend.warningEvent(utils.encodeQueryParams($stateParams));
+        return {events: [1,2,3,4,5],count:5}
     }
 
     /* @ngInject */
