5702208716577da96bf0d40c20d6b36ef978ffef
Myu <313231068@qq.com>
2016-05-17 09:57:51 +0800

    Update list.ctrl.js

--- glance/glance/applayout/list/list.ctrl.js
+++ glance/glance/applayout/list/list.ctrl.js
@@ -51,7 +51,7 @@
         function Stream(_callback) {
             var callback = _callback;
             var url = utils.buildFullURL("stack.sse")
-                + '?authorization=' + $rootScope.token;
+                + '?Authorization=' + $rootScope.token;
 
             events = new EventSource(url);
             events.addEventListener("deployment_process", function(event) {
