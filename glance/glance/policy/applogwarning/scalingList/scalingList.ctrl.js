f0a41baa5c168c22193f1fac68b285105930e70c
my9074 <313231068@qq.com>
2016-06-09 15:57:00 +0800

    1.Add scalingList for logwarning 2.Modify create app HOST radio logic

---
+++ glance/glance/policy/applogwarning/scalingList/scalingList.ctrl.js
@@ -0,0 +1,33 @@
+/**
+ * Created by geng on 16/5/18.
+ */
+(function () {
+    'use strict';
+    angular.module('glance.policy')
+        .controller('WarnLogScalingCtrl', WarnLogScalingCtrl);
+
+    /* @ngInject */
+    function WarnLogScalingCtrl(data, mdTable, $stateParams, $state) {
+        var self = this;
+
+        console.log($state);
+
+        self.scalingList = data.ScaleHistory;
+        self.count = data.Count;
+        self.WARNING_TYPE = WARNING_TYPE;
+        self.WARNING_RULE = WARNING_RULE;
+        self.table = mdTable.createTable('policy.tab.appwarning.logscaling');
+        self.searchKeyWord = $stateParams.keywords || '';
+
+        self.doSearch = doSearch;
+        ////
+        function doSearch(searchKeyWord) {
+            $state.go('policy.tab.appwarning.logscaling', {
+                page: 1,
+                per_page: 20,
+                keywords: searchKeyWord
+            }, {reload: true});
+        }
+
+    }
+})();
