d477e6be3c97e895a68ddebf1cf28fc409afa869
my9074 <313231068@qq.com>
2016-07-06 14:44:05 +0800

    1.Optimization image code, Remove image-build-setting.service.js 2.Support SVN for create image

--- glance/glance/image/create/create.ctrl.js
+++ glance/glance/image/create/create.ctrl.js
@@ -4,8 +4,11 @@
         .controller('ImageCreateCtrl', ImageCreateCtrl);
 
     /* @ngInject */
-    function ImageCreateCtrl(imageBackend, $rootScope, $scope, $state, imageBuildSetting, Notification) {
+    function ImageCreateCtrl(imageBackend, $rootScope, $scope, $state, Notification) {
         var self = this;
+        self.GIT = 0;
+        self.SVN = 1;
+
         ///
         self.form = {
             name: "",
@@ -15,7 +18,8 @@
             period: 5,
             description: "",
             branch: "",
-            imageName: ""
+            imageName: "",
+            repoType: 0
         };
 
         self.periodList = [
@@ -35,20 +39,20 @@
 
         self.projectsNameList = [];
         self.projectsImagesNameList = [];
-        self.triggerCount = 1;
         self.tag = true;
         self.branch = false;
 
         self.createProject = createProject;
-        self.triggerRules = triggerRules;
+        self.countTrigType = countTrigType;
+        self.modeChange = modeChange;
 
         activate();
 
-        function activate(){
+        function activate() {
             listProjectsName()
         }
 
-        function listProjectsName(){
+        function listProjectsName() {
             imageBackend.listProjects().then(function (data) {
                 angular.forEach(data.Project, function (value, index) {
                     self.projectsNameList.push(value.name);
@@ -58,15 +62,33 @@
         }
 
         function createProject(fromData) {
+            console.log(fromData)
             imageBackend.createProject(fromData, $scope.staticForm).then(function (data) {
                 Notification.success('项目创建成功');
-                $state.go('image.detail.version',({projectId: data.id}))
+                $state.go('image.detail.version', ({projectId: data.id}))
             })
         }
 
-        function triggerRules(checkValue) {
-            self.triggerCount = imageBuildSetting.triggerCheck(checkValue, self.triggerCount);
-            self.form.triggerType = imageBuildSetting.triggerRules(self.tag, self.branch);
+        function countTrigType() {
+            if (self.tag && self.branch) {
+                self.form.triggerType = IMAGE_TRIGGER_TYPE.SELECT_ALL
+            } else if (self.tag) {
+                self.form.triggerType = IMAGE_TRIGGER_TYPE.SELECT_TAG
+            } else if (self.branch) {
+                self.form.triggerType = IMAGE_TRIGGER_TYPE.SELECT_BRANCH
+            }
+        }
+
+        function modeChange(repoType) {
+            if (repoType === self.SVN) {
+                self.tag = false;
+                self.branch = true;
+            } else {
+                self.tag = true;
+                self.branch = false;
+            }
+
+            countTrigType()
         }
     }
 })();
